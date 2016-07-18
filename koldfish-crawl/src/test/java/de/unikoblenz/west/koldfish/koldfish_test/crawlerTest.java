8ef2280454c95892784a58d45721e85724c6de3a
rahul <rahul@OM>
2016-07-18 08:27:11 +0200

    Crawler Test case

--- koldfish-crawl/src/test/java/de/unikoblenz/west/koldfish/koldfish_test/crawlerTest.java
+++ koldfish-crawl/src/test/java/de/unikoblenz/west/koldfish/koldfish_test/crawlerTest.java
@@ -1,39 +1,28 @@
 package de.unikoblenz.west.koldfish.koldfish_test;
 
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-
-import de.unikoblenz.west.koldfish.frontier.BasicFrontier;
-import de.unikoblenz.west.koldfish.frontier.Frontier;
+import java.util.Scanner;
+import de.unikoblenz.west.koldfish.dictionary.Dictionary;
 import junit.framework.TestCase;
 
+public class crawlerTest extends TestCase {
 
-public class crawlerTest extends TestCase{
+	@SuppressWarnings("resource")
 	public void testcrawler() throws Exception {
-	Frontier frontier = new BasicFrontier();
-	//List<Long[]> data = new LinkedList<Long[]>();
-	long[] derefResponse = {1L,2L, 3L};
-	long[] derefResponse1 = {3L,5L, 1L};
-	//data.add(set1);
-	//data.add(set2);
-//	frontier.addAll(derefResponse);
-//	frontier.addAll(derefResponse1);
-	Long url1 = 1L;
-	Long url2 = 2L;
-	Long url3 = 3L;
-	Long url4 = 5L;
-	Set<Long> urlSet = new HashSet<Long>();
-	urlSet.add(url2);
-	urlSet.add(url1);
-	urlSet.add(url3);
-	urlSet.add(url4);
-	assertEquals(urlSet, frontier.getData());
-	
-	
-	
+		Dictionary dictionary = new Dictionary();
+		List<Long> expected = Arrays.asList(-588153461L, -1209624855L);
+		List<Long> actual = null;
+		Scanner s = new Scanner("/home/rahul/Desktop/seedlist.txt");
+
+		while (s.hasNextLine()) {
+
+			actual = dictionary.convertIris(Arrays.asList(s.nextLine()));
+		}
+		for (int i = 0; i < expected.size(); i++)
+			if (i < actual.size())
+				assertEquals(expected.get(i), actual.get(i));
+
 	}
-	
 
 }
